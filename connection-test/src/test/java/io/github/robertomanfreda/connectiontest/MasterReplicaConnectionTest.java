8bdd5dde8b2753444a820812b86f86ce9c5bcf36
Roberto Manfreda <robertomanfreda1994@gmail.com>
2025-02-27 11:50:39 +0100

    - Docker net for master-replica

--- connection-test/src/test/java/io/github/robertomanfreda/connectiontest/MasterReplicaConnectionTest.java
+++ connection-test/src/test/java/io/github/robertomanfreda/connectiontest/MasterReplicaConnectionTest.java
@@ -6,16 +6,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import static io.lettuce.core.ReadFrom.REPLICA_PREFERRED;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@EnabledIfSystemProperty(named = "spring.profiles.active", matches = "master-replica")
+@EnabledIfSystemProperty(named = "spring.profiles.active", matches = "standalone")
 @SpringBootTest
-public class StandaloneConnectionTest {
+public class MasterReplicaConnectionTest {
 
     private LettuceConnectionFactory factory;
     private RedisTemplate<String, String> redisTemplate;
@@ -25,7 +27,11 @@ public class StandaloneConnectionTest {
         RedisStandaloneConfiguration serverConfig = new RedisStandaloneConfiguration("localhost", 6379);
         serverConfig.setPassword("foobar");
 
-        factory = new LettuceConnectionFactory(serverConfig);
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .readFrom(REPLICA_PREFERRED)
+                .build();
+
+        factory = new LettuceConnectionFactory(serverConfig, clientConfig);
 
         redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(factory);
